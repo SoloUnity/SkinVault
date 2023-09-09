fc8d64195b87ea47b721c491340099d9b6cd147a
Gordon <GordonAccount@yahoo.com>
2023-09-08 22:35:56 -0400

    Initial Commit

---
+++ SkinVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkinVault
+//
+//  Created by Gordon on 2023-09-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkinVault/SkinVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinVaultApp.swift
+//  SkinVault
+//
+//  Created by Gordon on 2023-09-08.
+//
+
+import SwiftUI
+
+@main
+struct SkinVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
